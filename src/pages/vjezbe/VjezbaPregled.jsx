6db89a915f2d90c76079f53ca71574c3f83795a9
vladogeic <vlado.geic@gmail.com>
2026-03-23 17:06:21 +0100

    Update VjezbaPregled.jsx

--- src/pages/vjezbe/VjezbaPregled.jsx
+++ src/pages/vjezbe/VjezbaPregled.jsx
@@ -40,6 +40,7 @@ export default function VjezbaPregled() {
                         displayType={'text'}
                         />
                 </td>
+                </tr>
 
                 ))}
                 </tbody>
