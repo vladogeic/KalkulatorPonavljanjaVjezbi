6342a0762d2d1a24b1188d933873a3851dce5de5
vladogeic <vlado.geic@gmail.com>
2026-04-13 18:39:16 +0200

    Priprema podataka

--- src/services/treninzi/TreningPodaci.js
+++ src/services/treninzi/TreningPodaci.js
@@ -4,26 +4,31 @@ export const treninzi = [
     {
         sifra: 1,
         naziv: 'Trenig elitne razine',
-        korisnik: 1
+        korisnik: 1,
+        vjezbe:[1,4]
     },
     {
         sifra: 2,
         naziv: 'Napredni trening',
-        korisnik: 1
+        korisnik: 1,
+        vjezbe:[2,4]
     },
     {
         sifra: 3,
         naziv: 'Trening srednjeg intenziteta',
-        korisnik: 2
+        korisnik: 2,
+        vjezbe:[1,3,5]
     },
     {
         sifra: 4,
         naziv: 'Jači početnički trening',
-        korisnik: 3
+        korisnik: 3,
+        vjezbe:[2,3]
     },
     {
         sifra: 5,
         naziv: 'Početnički trening',
-        korisnik: 4
+        korisnik: 4,
+        vjezbe: [1,2,3,4,5]
     }
 ]
