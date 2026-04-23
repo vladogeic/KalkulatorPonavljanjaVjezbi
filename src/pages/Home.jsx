be74baab2c899b99767fc05ed8a22a2b93ea169d
edunovaProgramiranje <84087934+edunovaProgramiranje@users.noreply.github.com>
2026-04-23 19:48:10 +0200

    Pomoć u prikazu animiranih podataka na home stranici

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,16 +11,16 @@ import VjezbaService from "../services/vjezbe/VjezbaService";
 export default function Home() {
     const [brojTreninga, setBrojTrening] = useState(0);
     const [animatedTreninga, setAnimatedTreninga] = useState(0);
-    const [brojKorisnika,setBrojKorisnik] = useState(0);
+    const [brojKorisnika, setBrojKorisnika] = useState(0);
     const [animatedKorisnika, setAnimatedKorisnika] = useState(0);
-    const [brojVjezbi, setBrojVjezbe] = useState(0);
-    const [animatedVježbi, setAnimatedKorisnika] = useState(0)
+    const [brojVjezbi, setBrojVjezbi] = useState(0);
+    const [animatedVjezbi, setAnimatedVjezbi] = useState(0)
 
     useEffect(() => {
         const fetchData = async () => {
             try {
                 const treninzi = await TreningService.get();
-                
+
                 setBrojTrening(treninzi.data.length);
             } catch (error) {
                 console.error('Greška pri dohvaćanju podataka:', error);
@@ -30,7 +30,7 @@ export default function Home() {
         fetchData();
     }, []);
 
-   
+
     useEffect(() => {
         if (animatedTreninga < brojTreninga) {
             const timer = setTimeout(() => {
@@ -46,7 +46,7 @@ export default function Home() {
         const fetchData = async () => {
             try {
                 const korisnici = await KorisnikService.get();
-                
+
                 setBrojKorisnik(korisnici.data.length);
             } catch (error) {
                 console.error('Greška pri dohvaćanju podataka:', error);
@@ -56,7 +56,7 @@ export default function Home() {
         fetchData();
     }, []);
 
-   
+
     useEffect(() => {
         if (animatedKorisnika < brojKorisnika) {
             const timer = setTimeout(() => {
@@ -71,8 +71,8 @@ export default function Home() {
         const fetchData = async () => {
             try {
                 const vjezbe = await VjezbaService.get();
-                
-                setBrojVjezba(vjezbe.data.length);
+
+                setBrojVjezbi(vjezbe.data.length);
             } catch (error) {
                 console.error('Greška pri dohvaćanju podataka:', error);
             }
@@ -81,7 +81,7 @@ export default function Home() {
         fetchData();
     }, []);
 
-   
+
     useEffect(() => {
         if (animatedVjezbi < brojVjezbi) {
             const timer = setTimeout(() => {
@@ -92,64 +92,68 @@ export default function Home() {
     }, [animatedVjezbi, brojVjezbi]);
 
 
-      
-           
-   
-   
-   
-   
+
+
+
+
+
+
     return (
         <>
+            <Row>
+                <Col md={6}>
+                    <div style={{ textAlign: 'center' }}>
+                        <img src={slika} />
+                    </div>
+                    <p className="lead m-5 text-center">Dobrodošli na {IME_APLIKACIJE}</p>
+                    <div style={{ maxWidth: '500px', margin: 'auto' }}>
+                        <DotLottieReact
+                            src="/AISpark_InteractiveAssistant.lottie"
+                            loop
+                            autoplay
+                        />
+                    </div>
+                </Col>
 
-           <Row>
-            <Col md={6}>
-            <div style={{ textAlign: 'center' }}>
-                <img src={slika} />
+                <Col className="d-flex align-items-center justify-content-center">
+                    <div style={{ width: '100%', maxWidth: '400px' }}>
+                        <Card className="mb-3 shadow-lg border-0 statistikaPanel">
+                            <Card.Body className="text-center">
+                                <p className="text-white">Treninzi</p>
+                                <div className="statistikaTekst">
+                                    {animatedTreninga}
+                                </div>
+                            </Card.Body>
+                        </Card>
+                    </div>
+                </Col>
 
-            </div>
-            <p className="lead m-5 text-center">Dobrodošli na {IME_APLIKACIJE}</p>
-             <div style={{maxWidth: '500px', margin: 'auto'}}>
-                <DotLottieReact
-                    src="/AISpark_InteractiveAssistant.lottie"
-                     loop
-                    autoplay
-                />
-            </div>
-            </Col>
-           <Row
-            <Col className="d-flex align-items-center justify-content-center">
-                <div style={{ width: '100%', maxWidth: '400px' }}>
-                    <Card className="mb-3 shadow-lg border-0 statistikaPanel">
-                        <Card.Body className="text-center">
-                            <p className="text-white">Treninzi</p>
-                            <div className="statistikaTekst">
-                                {animatedTreninga}
+                <Col className="d-flex align-items-center justify-content-center">
+                    <div style={{ width: '100%', maxWidth: '400px' }}>
+                        <Card className="mb-3 shadow-lg border-0 statistikaPanel">
+                            <Card.Body className="text-center">
+                                <p className="text-white">Korisnici</p>
+                                <div className="statistikaTekst">
+                                    {animatedKorisnika}
+                                </div>
+                            </Card.Body>
+                        </Card>
+                    </div>
+                </Col>
 
-                                <Col className="d-flex align-items-center justify-content-center">
-                <div style={{ width: '100%', maxWidth: '400px' }}>
-                    <Card className="mb-3 shadow-lg border-0 statistikaPanel">
-                        <Card.Body className="text-center">
-                            <p className="text-white">Korisnici</p>
-                            <div className="statistikaTekst">
-                                {animatedKorisnika}
-
-
-                                <Col className="d-flex align-items-center justify-content-center">
-                <div style={{ width: '100%', maxWidth: '400px' }}>
-                    <Card className="mb-3 shadow-lg border-0 statistikaPanel">
-                        <Card.Body className="text-center">
-                            <p className="text-white">Vježbe</p>
-                            <div className="statistikaTekst">
-                                {animatedVježbi}
-            
-            
-                           </div>
-                        </Card.Body>
-                    </Card>
-                   
-                </div>
-            </Col>
-        </Row>
+                <Col className="d-flex align-items-center justify-content-center">
+                    <div style={{ width: '100%', maxWidth: '400px' }}>
+                        <Card className="mb-3 shadow-lg border-0 statistikaPanel">
+                            <Card.Body className="text-center">
+                                <p className="text-white">Vježbe</p>
+                                <div className="statistikaTekst">
+                                    {animatedVjezbi}
+                                </div>
+                            </Card.Body>
+                        </Card>
+                    </div>
+                </Col>
+            </Row>
         </>
     )
 
@@ -157,6 +161,6 @@ export default function Home() {
 
 
 
-            
-        
+
+
 }
