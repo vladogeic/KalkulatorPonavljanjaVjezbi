f9ac5491965b443775c89cbd55955fc9572e2e06
vladogeic <vlado.geic@gmail.com>
2026-05-05 17:43:47 +0200

    Update VjezbaPodaci.js

--- src/services/vjezbe/VjezbaPodaci.js
+++ src/services/vjezbe/VjezbaPodaci.js
@@ -3,40 +3,56 @@ export const vjezbe = [
         sifra: 1,
         naziv: 'Squat',
         opis: 'Opis',
-        koeficijent: 0.9
+       // koeficijent: 3    x bodyweight = Trening elitne razine    ocijena 5
+       // koeficijent  2,5  x bodyweight = Napredni trening         ocijena 4   
+      //  koeficijent  2    x bodyweight = Trening srednje razine   ocijena 3
+       // koeficijent  1,5  x bodyweight = Jača početnička razina   ocijena 2
+       // koeficijent  1    x bodyweight = početnički trening       ocijena 1
+
+
+       koeficijent 0.85
+
+
+
+
     },
 
     {
         sifra: 2,
         naziv: 'Bench press',
         opis: 'Opis',
-        koeficijent: 0.9
+       
+        koeficijent: 0,66
+        
+
+
+
     },
 
     {
         sifra: 3,
         naziv: 'Shoulder press',
         opis: 'Opis',
-        koeficijent: 0.9
+        koeficijent: 0.43
     },
     {
         sifra: 4,
         naziv: 'Dead lift',
         opis: 'Opis',
-        koeficijent: 0.9
+        koeficijent:  1
     }
     ,
     {
         sifra: 5,
         naziv: 'Dips',
         opis: 'Opis',
-        koeficijent: 0.9
+        koeficijent: 
     }
     ,
     {
         sifra: 6,
         naziv: 'Push ups',
         opis: 'Opis',
-        koeficijent: 0.9
+        koeficijent: 
     }
 ]
